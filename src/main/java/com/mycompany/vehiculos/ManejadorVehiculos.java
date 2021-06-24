a5345c7cdc81eaa0b5e7082d9438861b3ae5cc9f
Yefri Gonzalez <yefrig00@gmail.com>
2021-06-24 00:51:47 -0600

    Pom.xml fixed

--- src/main/java/com/mycompany/vehiculos/ManejadorVehiculos.java
+++ src/main/java/com/mycompany/vehiculos/ManejadorVehiculos.java
@@ -24,16 +24,19 @@ public class ManejadorVehiculos {
     public void verVehiculo(Vehiculo[] vehiculos){
         for(int i=0;i<vehiculos.length;i++){
             if(vehiculos[i] instanceof Camioneta){
-                System.out.println("Camioneta");
+                System.out.println("------------------------------");
+                System.out.println("\nCamioneta");
                 System.out.println(vehiculos[i].toString());
                 ((Camioneta)vehiculos[i]).trasportarPersonas();
             } else if(vehiculos[i] instanceof DeCarrera){
-                System.out.println("Auto de Carrera");
+                System.out.println("-------------------------------");
+                System.out.println("\nAuto de Carrera");
                 System.out.println(vehiculos[i].toString());
                 ((DeCarrera)vehiculos[i]).aplicarTurbo();
                 ((DeCarrera)vehiculos[i]).triplicarAceleracion();
             } else if(vehiculos[i] instanceof DeCarga){
-                System.out.println("Vehiculo de Carga");
+                System.out.println("---------------------------------");
+                System.out.println("\nVehiculo de Carga");
                 System.out.println(vehiculos[i].toString());
                 ((DeCarga)vehiculos[i]).transportarMaterial();
             }
